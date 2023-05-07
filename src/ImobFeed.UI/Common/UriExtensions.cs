1b840479bbe73788249f6e9226ee3c73befee9a2
Fabrício Godoy <skarllot@gmail.com>
2023-05-07 20:12:45 -0300

    Suporta abrir link e importar recomendação

---
+++ src/ImobFeed.UI/Common/UriExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ImobFeed.UI.Common;
+
+public static class UriExtensions
+{
+    public static void OpenInBrowser(this Uri uri)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            using var proc = new Process { StartInfo = { UseShellExecute = true, FileName = uri.ToString() } };
+            proc.Start();
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("x-www-browser", uri.ToString());
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", uri.ToString());
+            return;
+        }
+
+        throw new PlatformNotSupportedException(
+            $"Cannot open URI in browser for current platform: {RuntimeInformation.RuntimeIdentifier}");
+    }
+}
--- src/ImobFeed.UI/RecomendacaoTexto/RecomendacaoTextoViewModel.cs
+++ src/ImobFeed.UI/RecomendacaoTexto/RecomendacaoTextoViewModel.cs
@@ -1,9 +1,16 @@
-﻿using System.Reactive;
+﻿using System.IO.Abstractions;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using DynamicData.Binding;
+using ImobFeed.Api;
+using ImobFeed.Api.Recomendacoes;
+using ImobFeed.Core;
+using ImobFeed.Core.Common;
 using ImobFeed.Core.Referencia;
 using ImobFeed.Leitores.Texto;
+using ImobFeed.UI.Common;
 using NodaTime;
-using NodaTime.Extensions;
 using PropertyChanged.SourceGenerator;
 using ReactiveUI;
 
@@ -11,25 +18,57 @@ namespace ImobFeed.UI.RecomendacaoTexto;
 
 public partial class RecomendacaoTextoViewModel : RoutableViewModelBase
 {
+    private readonly IFileSystem _fileSystem;
+    private readonly IAppConfiguration _appConfig;
     private readonly IReferenciaAtivos _referenciaAtivos;
     [Notify] private DateTimeOffset _data = DateTimeOffset.Now;
+    [Notify] private ILeitorRecomendacao? _leitorRecomendacaoSelecionado;
+    [Notify] private string? _nomeCarteira;
+    [Notify] private string _conteudo = string.Empty;
+    private readonly ObservableProgress<ArquivoCriado> _output = new();
 
-    public RecomendacaoTextoViewModel(IScreen hostScreen, IReferenciaAtivos referenciaAtivos)
+    public RecomendacaoTextoViewModel(
+        IScreen hostScreen,
+        IFileSystem fileSystem,
+        IAppConfiguration appConfig,
+        IReferenciaAtivos referenciaAtivos)
         : base(hostScreen)
     {
+        _fileSystem = fileSystem;
+        _appConfig = appConfig;
         _referenciaAtivos = referenciaAtivos;
         Voltar = hostScreen.Router.NavigateBack;
 
-        Importar = ReactiveCommand.Create(
-            () =>
-            {
-            });
+        AbrirLink = ReactiveCommand.Create(
+            () => { new Uri(_leitorRecomendacaoSelecionado!.Url, UriKind.Absolute).OpenInBrowser(); },
+            this.WhenValueChanged(vm => vm.LeitorRecomendacaoSelecionado).Select(l => l is not null));
 
-        this.WhenActivated((CompositeDisposable disposables) => { });
+        Importar = ReactiveCommand.CreateFromTask(
+            () => Task.Run(ExecutarImportacao),
+            this.WhenValueChanged(vm => vm.Conteudo).Select(s => !string.IsNullOrWhiteSpace(s)));
+
+        this.WhenActivated(disposables => { _output.DisposeWith(disposables); });
     }
-    
+
     public ReactiveCommand<Unit, Unit> Voltar { get; }
+    public ReactiveCommand<Unit, Unit> AbrirLink { get; }
     public ReactiveCommand<Unit, Unit> Importar { get; }
 
     public IEnumerable<ILeitorRecomendacao> NomeCorretoras => ProvedorLeitorRecomendacao.TodosLeitores();
+
+    private void ExecutarImportacao()
+    {
+        if (_leitorRecomendacaoSelecionado is null)
+            return;
+
+        var dictAtivos = _referenciaAtivos.CarregarAtivos().Ativos.ToDictionary(x => x.Codigo);
+        var recomendacao = _leitorRecomendacaoSelecionado.Ler(
+            dictAtivos,
+            new StringReader(_conteudo),
+            string.IsNullOrWhiteSpace(_nomeCarteira) ? null : _nomeCarteira,
+            new YearMonth(_data.Year, _data.Month));
+
+        new ExportadorRecomendacao(_fileSystem, _appConfig)
+            .Salvar(recomendacao, _output);
+    }
 }
